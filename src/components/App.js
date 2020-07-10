28c5fb4f45ca70d96baafeda6b5229d3b35ae6ce
Marc Woolliscroft <mwoolli@gmail.com>
2020-07-10 13:31:35 -0700

    Add navigation between feed and submission components

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import CreateLink from './CreateLink'
+import Header from './Header'
+import { Switch, Route } from 'react-router';
+import LinkList from './LinkList';
 
 function App() {
   return (
-    <CreateLink />
+    <div className="center w85">
+      <Header />
+      <div className="ph3 pv1 background-gray">
+        <Switch>
+          <Route exact path="/" component={LinkList} />
+          <Route exact path="/create" component={CreateLink} />
+        </Switch>
+      </div>
+    </div>
   );
 }
 
