b32a8887d8ee90fb99cfc38c8196fabc4ceffaa7
Marc Woolliscroft <mwoolli@gmail.com>
2020-07-13 10:21:04 -0700

    Implement search using withApollo function

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,30 @@
 import React, { Component } from 'react'
 import { withApollo } from 'react-apollo'
-// import gql from 'graphql-tag'
+import gql from 'graphql-tag'
 import Link from './Link'
+
+const FEED_SEARCH_QUERY = gql`
+  query FeedSearchQuery($filter: String!) {
+    feed(filter: $filter) {
+      links {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+    }
+  }
+`
 
 class Search extends Component {
   state = {
@@ -28,7 +51,13 @@ class Search extends Component {
   }
 
   _executeSearch = async () => {
-    // implementation
+    const { filter } = this.state
+    const result = await this.props.client.query({
+      query: FEED_SEARCH_QUERY,
+      variables: { filter },
+    })
+    const links = result.data.feed.links
+    this.setState({ links })
   }
 }
 
